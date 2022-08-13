712eeb7143f1d6e57af47c2d3f6617a30604fc5c
min-gui <msj349911@gmail.com>
2022-08-13 23:32:35 +0900

    add hating other number

--- src/AdjacentTest.java
+++ src/AdjacentTest.java
@@ -1,87 +1,37 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class AdjacentTest {
     public static void main(String[] args) {
         AdjacentTest adjacentTest = new AdjacentTest();
-        adjacentTest.solution(4, new int[][]{{1, 2}, {2, 3}, {3, 4}});
+        System.out.println(adjacentTest.solution(new int[]{70, 50, 80, 50}
+                , 100));
+
 
     }
 
-    public int solution(int n, int[][] wires) {
-        int answer = n;
-        initGrape(n);
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
 
-        for (int[] item : wires) {
-            put(item[0], item[1]);
+        int upCnt = 0;
+        int downCnt = people.length-1;
+
+        while (downCnt-upCnt >= 0){
+            if (people[downCnt]+people[upCnt] <= limit){
+                downCnt--;
+                upCnt++;
+                answer++;
+            }else {
+                downCnt--;
+                answer++;
+            }
         }
 
-        for (int i = 0; i < wires.length; i++) {
-            delete(wires[i][0], wires[i][1]);
-
-            answer = Math.min(answer, Math.abs(search(n,wires[i][0]) - search(n,wires[i][1])));
-
-            put(wires[i][0], wires[i][1]);
-        }
-
-
-        System.out.println(answer);
         return answer;
-    }
-
-    private ArrayList<ArrayList<Integer>> listGraph;
-
-    public void initGrape(int initSize) {
-        this.listGraph = new ArrayList<ArrayList<Integer>>();
-
-        for (int i = 0; i <= initSize; i++) {
-            listGraph.add(new ArrayList<Integer>());
-        }
-    }
-
-
-
-    public int search(int n,int start) {
-        int cnt = 0;
-        boolean[] visited = new boolean[n + 1];;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-
-        visited[start] = true;
-        while (!queue.isEmpty()) {
-            int dot = queue.poll();
-            for (int i = 0; i < listGraph.get(dot).size(); i++) {
-                if (!visited[listGraph.get(dot).get(i)]) {
-                    visited[dot] = true;
-                    queue.add(listGraph.get(dot).get(i));
-                    cnt++;
-                }
-            }
-        }
-
-        return cnt;
-    }
-
-    public void printGraphToAdjList() {
-        for (int i = 1; i < listGraph.size(); i++) {
-            System.out.println("정점 " + i + "의 인접리스트");
-
-            for (int j = 0; j < listGraph.get(i).size(); j++) {
-                System.out.print(" -> " + listGraph.get(i).get(j));
-            }
-            System.out.println();
-        }
-    }
-
-    public void put(int x, int y) {
-        listGraph.get(x).add(y);
-        listGraph.get(y).add(x);
-    }
-
-    public void delete(int x, int y) {
-        listGraph.get(x).remove(Integer.valueOf(y));
-        listGraph.get(y).remove(Integer.valueOf(x));
     }
 
 
--- src/ProgramersTest.java
+++ src/ProgramersTest.java
@@ -1,57 +1,34 @@
 import java.util.*;
 
 public class ProgramersTest {
+
+    /**
+     * 1. 아이디어
+     *
+     *
+     */
     public static void main(String[] args) {
         ProgramersTest programersTest = new ProgramersTest();
-        System.out.println(programersTest.solution(
-                9, new int[][]{{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}}));
+        System.out.println(programersTest.solution(new int[]{1, 1, 1, 3, 1, 1, 1}));
     }
 
+    public int[] solution(int[] arr) {
 
-    int[][] map;
-
-    public int solution(int n, int[][] wires) {
-        int answer = n;
-        map = new int[n + 1][n + 1];
-        for (int i = 0; i < wires.length; i++) {
-            map[wires[i][0]][wires[i][1]] = 1;
-            map[wires[i][1]][wires[i][0]] = 1;
-        }
-
-        for (int i = 0; i < wires.length; i++) {
-            int a = wires[i][0];
-            int b = wires[i][1];
-
-            map[a][b] = 0;
-            map[b][a] = 0;
-            answer = Math.min(answer, serach(n,a));
-            map[a][b] = 1;
-            map[b][a] = 1;
-        }
-
-
-        return answer;
-    }
-
-    public int serach(int n, int start) {
-        boolean[] visited = new boolean[n + 1];
-        int cnt = 1;
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-
-        while (!queue.isEmpty()) {
-            int point = queue.poll();
-            visited[point] = true;
-
-            for (int i = 1; i <= n; i++) {
-                if (visited[i]) continue;
-                if (map[point][i] == 1) {
-                    queue.offer(i);
-                    cnt++;
-                }
+        List<Integer> list = new ArrayList<>();
+        int pop = 10;
+        for (int i : arr){
+            if (pop != i){
+                list.add(i);
+                pop = i;
             }
         }
-        return (int) Math.abs(n - 2 * cnt);
+
+        int[] answer = new int[list.size()];
+        for (int i =0 ; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
     }
 
 
