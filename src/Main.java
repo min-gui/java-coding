1db274b18c792cd7fba4dd2948cdb9b81a7665b1
min-gui <msj349911@gmail.com>
2022-08-21 10:02:44 +0900

    add stack

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,87 @@
-import javax.swing.text.html.Option;
-import java.util.*;
-
+import java.util.Scanner;
 public class Main {
-    static boolean[] visted;
-    static ArrayList<ArrayList<Integer>> arr;
+
+    public static int[] stack;
+    public static int size = 0;
+
+
     public static void main(String[] args) {
-        int answer = 0;
-        Scanner sc = new Scanner(System.in);
 
-        //정점의 갯수
-        int N = sc.nextInt();
-        //간선의 갯수
-        int M = sc.nextInt();
+        Scanner in = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 
-        visted = new boolean[N + 1];
-        arr = new ArrayList<>();
-        for (int i = 0; i < N + 1; i++) {
-            arr.add(new ArrayList<>());
-        }
-        for (int i = 0; i < M; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
+        int N = in.nextInt();
 
-            arr.get(u).add(v);
-            arr.get(v).add(u);
-        }
+        stack = new int[N];
 
-        for (int i = 1 ; i <= N ;i++){
-            if (!visted[i]){
-                search(i);
-                answer++;
+        for(int i = 0; i < N; i++) {
+
+            String str = in.next();
+
+            switch (str) {
+
+                case "push":
+                    push(in.nextInt());
+                    break;
+
+                case "pop":
+                    sb.append(pop()).append('\n');
+                    break;
+
+                case "size":
+                    sb.append(size()).append('\n');
+                    break;
+
+                case "empty":
+                    sb.append(empty()).append('\n');
+                    break;
+
+                case "top":
+                    sb.append(top()).append('\n');
+                    break;
             }
-        }
-        System.out.println(answer);
 
+        }
+        System.out.println(sb);
     }
 
+    public static void push(int item) {
+        stack[size] = item;
+        size++;
+    }
 
-    static public void search(int start){
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
+    public static int pop() {
+        if(size == 0) {
+            return -1;
+        }
+        else {
+            int res = stack[size - 1];
+            stack[size - 1] = 0;
+            size--;
+            return res;
+        }
+    }
 
-        while (!queue.isEmpty()){
-            int a = queue.poll();
-            visted[a] = true;
-            for (int i = 0 ; i < arr.get(a).size() ; i++){
-                int value = arr.get(a).get(i);
-                if (!visted[value]){
-                    visted[value] = true;
-                    queue.add(value);
-                }
+    public static int size() {
+        return size;
+    }
 
-            }
+    public static int empty() {
+        if(size == 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
 
+    public static int top() {
+        if(size == 0) {
+            return -1;
+        }
+        else {
+            return stack[size - 1];
         }
     }
 
 }
-
