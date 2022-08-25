35733004ca1cf8757a12519f0122022ea2b8cad6
min-gui <msj349911@gmail.com>
2022-08-25 10:03:02 +0900

    back 1107 리모컨

--- src/Main.java
+++ src/Main.java
@@ -2,94 +2,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
-    static char[][] map;
-    static boolean[][] vistied;
-    static int max = 0;
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int mapSize = sc.nextInt();
-        map = new char[mapSize][mapSize];
-        vistied = new boolean[mapSize][mapSize];
+        int cnt = 0;
+        int E = sc.nextInt();
+        int S = sc.nextInt();
+        int M = sc.nextInt();
 
+        while (true) {
+            cnt++;
+            int eNa = cnt % 15;
+            int sNa = cnt % 28;
+            int mNa = cnt % 19;
+            if (eNa == 0) eNa = 15 ;
+            if (sNa == 0) sNa = 28 ;
+            if (mNa == 0) mNa = 19 ;
 
-        for (int i = 0; i < mapSize; i++) {
-            String inputString = sc.next();
-            for (int j = 0; j < mapSize; j++) {
-                map[i][j] = inputString.charAt(j);
+            if (eNa == E && sNa == S && mNa == M){
+                break;
             }
         }
-
-        for (int i = 0; i < mapSize; i++) {
-            for (int j = 0; j < mapSize; j++) {
-                Point p1 = new Point(j, i);
-                search(p1);
-            }
-        }
-
-
-        System.out.println(max);
-
-    }
-
-    static void search(Point point) {
-        for (int i = 0; i < 4; i++) {
-            int x = point.x + dx[i];
-            int y = point.y + dy[i];
-            if ((x >= 0 && x < map.length) && (y >= 0 && y < map.length)) {
-                if (map[point.y][point.x] != map[y][x]) {
-                    char temp = map[point.y][point.x];
-                    map[point.y][point.x] = map[y][x];
-                    map[y][x] = temp;
-                    maxSearch();
-                    map[y][x] = map[point.y][point.x];
-                    map[point.y][point.x] = temp;
-                }
-            }
-        }
-    }
-
-    static void maxSearch() {
-
-        int sum = 1;
-        //필드만 최고 찾기
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length - 1; j++) {
-                if (map[i][j] == map[i][j + 1]) {
-                    sum++;
-                } else {
-                    sum = 1;
-                }
-                max = Math.max(max, sum);
-            }
-            sum = 1;
-        }
-        //열만 최고 찾기
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map.length - 1; j++) {
-                if (map[j][i] == map[j + 1][i]) {
-                    sum++;
-                } else {
-                    sum = 1;
-                }
-                max = Math.max(max, sum);
-            }
-            sum = 1;
-        }
-    }
-
-
-     static class Point {
-        int x;
-        int y;
-
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+        System.out.println(cnt);
 
     }
 }
