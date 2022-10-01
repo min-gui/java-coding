17db2873ed311ace0972273f2ceb4d36c17b949c
min-gui <msj349911@gmail.com>
2022-10-02 01:55:26 +0900

    back 1966

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,76 @@
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 
 public class Main {
 
-    /*
-    1. 아이디어
-    모든 순서가 나오게 출력.
-    하나의 열에 모든 수의 조함이 나와야된다.
+    /**
+     * 1. 아이디어
+     * 문서의 개수 N, 몇번째에 놓여 있는지 M, N개 문서의 중요도가 차례로 나온다.
+     * - 구현
+     * 구분표현을 만든다.
+     * 번지수, 중요도.
+     * 들어오는 가장 큰 수를 찾는다.
      */
 
     static int arr[];
     static boolean visited[];
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        arr = new int[M];
-        visited = new boolean[N];
+        int testCase = sc.nextInt();
 
-        dfs(0,N,M,0);
+        for (int i = 0; i < testCase; i++) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int MAX = 0;
+            int cnt = 0;
 
-    }
-
-
-    static void dfs(int start, int N, int M, int depth) {
-
-        if (M == depth) {
-            for (int i : arr) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-            return;
-        }
-
-        for (int i = start; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr[depth] = i + 1;
-                dfs(i + 1, N, M, depth+1);
-                visited[i] = false;
+            Queue<Point> queue = new LinkedList<>();
+            for (int j = 0; j < N; j++) {
+                int value = sc.nextInt();
+                MAX = Math.max(MAX, value);
+                queue.add(new Point(value, j));
             }
 
+            while (!queue.isEmpty()){
+
+                Point nowP = queue.poll();
+                boolean flag = true;
+
+                for (Point p : queue){
+                    if (nowP.value < p.value){
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag){
+                    cnt++;
+                    if (nowP.turn==M){
+                        System.out.println("cnt = " + cnt);
+                        break;
+                    }
+                }else {
+                    queue.add(nowP);
+                }
+            }
+
         }
-        
     }
+
+    public static class Point {
+        int value;
+        int turn;
+
+        public Point(int value, int turn) {
+            this.value = value;
+            this.turn = turn;
+        }
+    }
+
 
 }
