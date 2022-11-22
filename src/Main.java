b8106ff9081590b6ea220ba6be65239bc67037e8
min-gui <msj349911@gmail.com>
2022-11-22 11:37:55 +0900

    back 1987

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,60 @@ import java.util.*;
 
 public class Main {
 
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+    static boolean[] visted;
+    static int[][] map;
+    static int R = 0;
+    static int C = 0;
+    static int maxCnt = 0;
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int cnt = 1;
-        int N = sc.nextInt();
-        int[][] lectures = new int[N][2];
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        R = sc.nextInt();
+        C = sc.nextInt();
+        //아스키 코드 값 이용.
+        visted = new boolean[26];
+        map = new int[R][C];
 
-        for (int i = 0; i < N; i++) {
-            int si = sc.nextInt();
-            int ti = sc.nextInt();
-            lectures[i][0] = si;
-            lectures[i][1] = ti;
-        }
-
-        Arrays.sort(lectures, Comparator.comparing(o1 -> o1[0]));
-        //강의 끝나는 시간 추가.
-        queue.add(lectures[0][1]);
-        for (int i = 1; i < N; i++) {
-            if (queue.peek() <= lectures[i][0]){
-                queue.poll();
+        for (int i = 0; i < R; i++) {
+            String str = sc.next();
+            for (int j = 0; j < C; j++) {
+                map[i][j] = str.charAt(j) - 'A';
             }
-            queue.add(lectures[i][1]);
-
-
         }
-        System.out.println(queue.size());
+
+        dfs(new Horse(0, 0), 1);
+
+        System.out.println(maxCnt);
+    }
+
+    public static void dfs(Horse horse, int inCnt) {
+        int x = horse.x;
+        int y = horse.y;
+        visted[map[y][x]] = true;
+        maxCnt = Math.max(maxCnt,inCnt);
+        for (int i = 0; i < 4; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if ((0 <= nextX && nextX < C) && (0 <= nextY && nextY < R)) {
+                if (!visted[map[nextY][nextX]]) {
+                    dfs(new Horse(nextX, nextY), inCnt+1);
+                    visted[map[nextY][nextX]] = false;
+                }
+            }
+        }
+
+    }
+
+
+    static class Horse {
+        int x, y;
+
+        public Horse(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 
 }
