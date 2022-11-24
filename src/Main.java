df64c4d0b8db5250f8100691ec9a3b27716f0b7f
min-gui <msj349911@gmail.com>
2022-11-24 17:11:20 +0900

    codility binary

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,51 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    static int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
-    static int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
-    static boolean[][] visted;
-    static int[][] map;
-    static int testCase = 0;
-    static int I = 0;
-    static int minCnt = 0;
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        testCase = sc.nextInt();
-
-        for (int i = 0; i < testCase; i++) {
-            I = sc.nextInt();
-            visted = new boolean[I][I];
-            map = new int[I][I];
-            int x1 = sc.nextInt();
-            int y1 = sc.nextInt();
-
-            int x2 = sc.nextInt();
-            int y2 = sc.nextInt();
-            search(new Point(x1, y1), new Point(x2, y2));
-        }
-
+        solution(4);
     }
 
-    public static void search(Point start, Point end) {
+    public static int solution(int N) {
+        // write your code in Java SE 11
 
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(start);
+        int totalCnt = 0;
+        Point p = new Point();
+        while (true){
+            int na = N%2;
+            N = N/2;
 
-        while (!queue.isEmpty()) {
-            Point currentP = queue.poll();
 
-            if (currentP.x == end.x && currentP.y == end.y) {
-                System.out.println(map[currentP.y][currentP.x]);
-                return;
+            if (na == 1){
+                p.is = true;
+                totalCnt = Math.max(totalCnt, p.cnt);
+                p.cnt = -1;
+
             }
 
-            for (int i = 0; i < 8; i++) {
-                int nextX = currentP.x + dx[i];
-                int nextY = currentP.y + dy[i];
-                if (0 <= nextX && nextX < I && 0 <= nextY && nextY < I) {
-                    if (!visted[nextY][nextX]) {
-                        visted[nextY][nextX] = true;
-                        map[nextY][nextX] = map[currentP.y][currentP.x] + 1;
-                        queue.add(new Point(nextX, nextY));
-                    }
-                }
+            if(p.is == true){
+                p.cnt++;
             }
-        }
-    }
-    static class Point {
-        int x, y;
 
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+            if (N == 1){
+                totalCnt = Math.max(totalCnt, p.cnt);
+                break;
+            }
 
+        }
+
+        System.out.println(+totalCnt);
+        return 0;
+    }
+    public static class Point{
+        int cnt=0;
+        //셀수 있는지 이전에 1이 나왔었는지 판별
+        boolean is = false;
+    }
 }
-
-
-
-
