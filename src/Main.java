8aac995d4c21a6b915a94633ccd4f268615f83a6
min-gui <msj349911@gmail.com>
2022-12-08 14:00:35 +0900

    back 11752

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        int[] test = {3, 7, 4, 6, 5};
+    static int N;
+    static ArrayList<Integer>[] list;
+    static int[] parents;
+    static boolean[] visited;
 
-        System.out.println(maxSubsetSum(test));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        list = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        parents = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            //양쪽의 값을 넣어줘 연결되어있음을 표현한다.
+            list[x].add(y);
+            list[y].add(x);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            //방문하지 않은 곳이라면 색
+            //두번째 노드 부터 차례대로 탐색한다.
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+
+        for (int i = 1; i <= N ; i++){
+            System.out.println(parents[i]);
+        }
+
     }
 
-    static int maxSubsetSum(int[] arr) {
-        int length = arr.length;
-
-        if (length == 1) {
-            return arr[0];
-        } else if (length == 2) {
-            return arr[1] >= arr[0] ? arr[1] : arr[0];
+    private static void dfs(int x) {
+        if (visited[x]) {
+            return;
         }
-        int[] tempArr = new int[length + 1];
-        int totalCnt = -100000;
-        tempArr[0] = -100000;
-        tempArr[1] = arr[0];
-        tempArr[2] = arr[1];
+        visited[x] = true;
 
-        for (int i = 2; i < length; i++) {
-            int tempMax = Math.max(tempArr[i - 1], tempArr[i - 2]);
-            if (arr[i] >= 0) {
-                tempArr[i + 1] = Math.max(tempMax + arr[i], arr[i]);
-            } else {
-                tempArr[i + 1] = tempMax;
+        for (int i : list[x]) {
+            //아직 방문하지 않았으면.
+            if (!visited[i]) {
+                //부모노드 추가
+                parents[i] = x;
+                //자식노드 탐색.
+                dfs(i);
             }
-            totalCnt = Math.max(tempArr[i + 1],totalCnt);
         }
 
-        return totalCnt;
     }
 
 }
