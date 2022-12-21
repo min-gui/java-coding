f8806f8e24174496aa504e5c6c46e2cac6fc83de
min-gui <msj349911@gmail.com>
2022-12-21 12:03:43 +0900

    back 17413 단어뒤집기

---
+++ src/BackTestMain.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BackTestMain {
+    static Stack<Character> characterStack;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String strings = br.readLine();
+        boolean continueIs = false;
+        characterStack = new Stack<>();
+
+        for (int i = 0; i < strings.length(); i++) {
+            char charIndex = strings.charAt(i);
+            //"<" 가 시작 되었고, 현재 인덱스가 ">" 가 아닐때 가로안에 있는 문자열 하나씩 출력.
+            if (continueIs && charIndex != '>') {
+                System.out.print(charIndex);
+                continue;
+            }
+
+
+            if (charIndex == '<') {
+                //문자열이 < 로시작할때 스택 안에 문자열이 있으면 모두 출력.
+                continueIs = true;
+                stackPrint();
+                System.out.print("<");
+            } else if (charIndex == '>') {
+                continueIs = false;
+                System.out.print(">");
+            } else if (charIndex != ' ') {
+                //인덱스가 <,>,' ' 아닐때 스텍에 문자열 추가.
+                characterStack.add(charIndex);
+            }
+
+
+            //인텍스가 ' '이거나 마지막줄 일때
+            if (charIndex == ' ' || i == strings.length() - 1) {
+                stackPrint();
+
+                if (charIndex == ' ')
+                    System.out.print(" ");
+            }
+        }
+    }
+
+    public static void stackPrint(){
+        while (!characterStack.isEmpty()) {
+            char c = characterStack.pop();
+            System.out.print(c);
+        }
+    }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,67 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static ArrayList<Integer>[] list;
-    static int[] parents;
-    static boolean[] visited;
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        list = new ArrayList[N + 1];
-        visited = new boolean[N + 1];
-        parents = new int[N + 1];
 
-        for (int i = 1; i <= N; i++) {
-            list[i] = new ArrayList<>();
-        }
-
-        for (int i = 1; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-
-            //양쪽의 값을 넣어줘 연결되어있음을 표현한다.
-            list[x].add(y);
-            list[y].add(x);
-        }
-
-        for (int i = 1; i <= N; i++) {
-            //방문하지 않은 곳이라면 색
-            //두번째 노드 부터 차례대로 탐색한다.
-            if (!visited[i]) {
-                dfs(i);
-            }
-        }
-
-        for (int i = 1; i <= N ; i++){
-            System.out.println(parents[i]);
-        }
-
+        solution(7);
+        int[][] arr = {{1,2},{1,3}};
+        String[] strarr = {"root","cs","hello"};
+        solution(3,arr,strarr);
     }
 
-    private static void dfs(int x) {
-        if (visited[x]) {
-            return;
-        }
-        visited[x] = true;
+    public static int solution(int N, int[][] relation, String[] dirname) {
+        ArrayList<Point> arrayList = new ArrayList<>();
+        for ( int i =0 ; i < N-1; i ++){
+            int start = relation[i][1];
+            int end = relation[i][1];
+            String name = dirname[i+1];
 
-        for (int i : list[x]) {
-            //아직 방문하지 않았으면.
-            if (!visited[i]) {
-                //부모노드 추가
-                parents[i] = x;
-                //자식노드 탐색.
-                dfs(i);
-            }
+            arrayList.add(new Point(start,end, name));
+
         }
 
+        
+
+
+        return 0;
+    }
+
+    static class Point{
+        int start;
+        int end;
+        String name;
+
+        public Point(int start, int end, String name) {
+            this.start = start;
+            this.end = end;
+            this.name = name;
+        }
+    }
+
+    public static long[] solution(long n) {
+        long min = 1;
+        if ((n / 7) > 0) {
+            min = (n / 7) * 2;
+        }
+
+        long na = 1;
+        if (n % 7 == 0 ){
+            na = 0;
+        } else if (n % 7 > 1 && 7 < n) {
+            na = 2;
+        }
+
+        long max = na + min;
+
+        System.out.println(max);
+
+        long[] answer = {min, max};
+
+        return answer;
     }
 
 }
