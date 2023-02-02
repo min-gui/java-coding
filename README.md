09651526a14a9cbb04c141523a75ee7503717c7d
min-gui <msj349911@gmail.com>
2023-02-02 23:40:56 +0900

    쇠막대기 10799

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-##coding test
+# Coding Test
--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,41 @@
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
 
-    public static void main(String[] args) {
 
+    //1. 쇠막대기 겹치지 않게 아래에 자기보다 큰거.
+    //2. 끝점이 겹치지 않게, 레이저는 적어도 하나 쏜다.
 
-        solution(7);
-        int[][] arr = {{1,2},{1,3}};
-        String[] strarr = {"root","cs","hello"};
-        solution(3,arr,strarr);
-    }
+    /**
+     * 설계 .
+     * () 는 레이저이다. 예) (())레이저 하나 쇠막대기 두개.
+     * @param args
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static int solution(int N, int[][] relation, String[] dirname) {
-        ArrayList<Point> arrayList = new ArrayList<>();
-        for ( int i =0 ; i < N-1; i ++){
-            int start = relation[i][1];
-            int end = relation[i][1];
-            String name = dirname[i+1];
+        char[] result = br.readLine().toCharArray();
+        Stack<Character> stack = new Stack<>();
+        int total =0;
 
-            arrayList.add(new Point(start,end, name));
+        for (int i = 0; i < result.length; i++){
+            if (result[i] == '(')
+                stack.push(result[i]);
+            else {
+                stack.pop();
+                if (result[i-1]=='(')
+                    total += stack.size();
+                else
+                    total++;
+            }
 
         }
-
-        
-
-
-        return 0;
+        System.out.println(total);
     }
 
-    static class Point{
-        int start;
-        int end;
-        String name;
 
-        public Point(int start, int end, String name) {
-            this.start = start;
-            this.end = end;
-            this.name = name;
-        }
-    }
-
-    public static long[] solution(long n) {
-        long min = 1;
-        if ((n / 7) > 0) {
-            min = (n / 7) * 2;
-        }
-
-        long na = 1;
-        if (n % 7 == 0 ){
-            na = 0;
-        } else if (n % 7 > 1 && 7 < n) {
-            na = 2;
-        }
-
-        long max = na + min;
-
-        System.out.println(max);
-
-        long[] answer = {min, max};
-
-        return answer;
-    }
 
 }
