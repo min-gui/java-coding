5cf1130fdaf817caabfa73a2d2d56108f31113c2
min-gui <msj349911@gmail.com>
2023-02-26 23:21:19 +0900

    추이성

---
+++ src/effective/ch3/item10/composition/ColorPoint.java
@@ -0,0 +1,34 @@
+package effective.ch3.item10.composition;
+
+import effective.ch3.item10.Color;
+import effective.ch3.item10.Point;
+
+import java.util.Objects;
+
+public class ColorPoint {
+    private final Point point;
+    private final Color color;
+
+    public ColorPoint(int x, int y, Color color) {
+        point = new Point(x, y);
+        this.color = Objects.requireNonNull(color);
+    }
+
+    /**
+     * 이 ColorPoint의 Point 뷰를 반환한다.
+     */
+    public Point asPoint() {
+        return point;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (!(o instanceof ColorPoint))
+            return false;
+        ColorPoint cp = (ColorPoint) o;
+        return cp.point.equals(point) && cp.color.equals(color);
+    }
+
+    @Override public int hashCode() {
+        return 31 * point.hashCode() + color.hashCode();
+    }
+}
---
+++ src/effective/ch3/item10/ingeritance/ColorPoint.java
@@ -0,0 +1,51 @@
+package effective.ch3.item10.ingeritance;
+
+import effective.ch3.item10.Point;
+
+import java.awt.*;
+
+public class ColorPoint extends Point {
+
+    private final Color color;
+
+    public ColorPoint(int x, int y, Color color) {
+        super(x, y);
+        this.color = color;
+    }
+
+    // 코드 10-2 잘못된 코드 - 대칭성 위배! (57쪽)
+    // 컬러 정보는 무시하고 있다.
+    @Override public boolean equals(Object o) {
+        if (!(o instanceof ColorPoint))
+            return false;
+        return super.equals(o) && ((ColorPoint) o).color == color;
+    }
+
+    //    // 코드 10-3 잘못된 코드 - 추이성 위배! (57쪽)
+//    @Override public boolean equals(Object o) {
+//        if (!(o instanceof Point))
+//            return false;
+//
+//        // o가 일반 Point면 색상을 무시하고 비교한다.
+//        if (!(o instanceof ColorPoint))
+//            return o.equals(this);
+//
+//        // o가 ColorPoint면 색상까지 비교한다.
+//        return super.equals(o) && ((ColorPoint) o).color == color;
+//    }
+
+    //구체 클래스를 확장해 새로운 값을 추가하면서 equals 규약을 만족시킬 방법은 존재하지 않는다.
+    public static void main(String[] args) {
+        // 첫 번째 equals 메서드(코드 10-2)는 대칭성을 위배한다. (57쪽)
+        Point p = new Point(1, 2);
+        ColorPoint cp = new ColorPoint(1, 2, Color.RED);
+        System.out.println(p.equals(cp) + " " + cp.equals(p));
+
+        // 두 번째 equals 메서드(코드 10-3)는 추이성을 위배한다. (57쪽)
+        ColorPoint p1 = new ColorPoint(1, 2, Color.RED);
+        Point p2 = new Point(1, 2);
+        ColorPoint p3 = new ColorPoint(1, 2, Color.BLUE);
+        System.out.printf("%s %s %s%n",
+                p1.equals(p2), p2.equals(p3), p1.equals(p3));
+    }
+}
