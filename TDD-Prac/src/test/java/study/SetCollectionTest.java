0b18b5fdc28bd773773394c41cc64ca8bba5b580
min-gui <msj349911@gmail.com>
2024-09-13 00:04:15 +0900

    feat : set collection 크기 확인

--- TDD-Prac/src/test/java/study/SetCollectionTest.java
+++ TDD-Prac/src/test/java/study/SetCollectionTest.java
@@ -1,4 +1,36 @@
 package study;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@DisplayName("Set Collection 학습")
 public class SetCollectionTest {
+
+    private Set<Integer> integerSet;
+
+    //각 메서드 실행 되기 전에 한번 실행된다.
+    @BeforeEach
+    void beforeEach(){
+        integerSet = new HashSet<>();
+        integerSet.add(1);
+        integerSet.add(2);
+        integerSet.add(2);
+    }
+
+
+    @Test
+    @DisplayName("Set 크기 확인")
+    public void setTest() throws Exception {
+
+        Assertions.assertThat(integerSet.size()).isEqualTo(2);
+
+    }
+
+
+
 }
