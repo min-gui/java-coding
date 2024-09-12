59f0599e6db9ab6a6b1e5fecde0c5ef845f42f78
min-gui <msj349911@gmail.com>
2024-09-12 23:29:05 +0900

    feat : springClass split 검증

---
+++ TDD-Prac/src/test/java/study/StringClassTest.java
@@ -0,0 +1,34 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("String 클래스 테스트")
+public class StringClassTest {
+
+    @Test
+    @DisplayName("1,2을 분리학 불리된 결과가 [\"1\", \"2\"] 인지 판다")
+    public void splitTest() throws Exception {
+
+        String[] split = "1,2".split(",");
+
+        Assertions.assertThat(split).containsExactly("1", "2");
+
+
+     }
+
+     @Test
+     @DisplayName(", 가 없을때도 정상적으로 변환 확인.")
+     public void spritTest02() throws Exception {
+
+         String[] split = "1".split(",");
+
+         Assertions.assertThat(split).containsExactly("1");
+
+      }
+
+
+
+
+}
