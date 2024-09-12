23eadbffec1fd04e3341d71c723aee6f5b32a2ff
min-gui <msj349911@gmail.com>
2024-09-12 23:48:02 +0900

    feat : exception 에러 검증

--- TDD-Prac/src/test/java/study/StringClassTest.java
+++ TDD-Prac/src/test/java/study/StringClassTest.java
@@ -16,29 +16,46 @@ public class StringClassTest {
         Assertions.assertThat(split).containsExactly("1", "2");
 
 
+    }
+
+    @Test
+    @DisplayName(", 가 없을때도 정상적으로 변환 확인.")
+    public void spritTest02() throws Exception {
+
+        String[] split = "1".split(",");
+
+        Assertions.assertThat(split).containsExactly("1");
+
+    }
+
+    @Test
+    @DisplayName("substring 테스트")
+    public void substringTest() throws Exception {
+
+        String ex = "abc";
+        String result = "abc".substring(1, ex.length() - 1);
+        Assertions.assertThat(result).isEqualTo("b");
+
+    }
+
+    @Test
+    @DisplayName("특정인덱스 가져오기 charAt")
+    public void charAtTest() throws Exception {
+
+        char c = "abc".charAt(0);
+        Assertions.assertThat(c).isEqualTo('a');
+
+    }
+
+    @Test
+    @DisplayName("인덱스 벗어나면 벗어난 ")
+    public void charAtTest02() throws Exception {
+
+        Assertions.assertThatThrownBy(() -> "abc".charAt(8))
+                .isExactlyInstanceOf(StringIndexOutOfBoundsException.class)
+                .isInstanceOf(RuntimeException.class);
+
      }
-
-     @Test
-     @DisplayName(", 가 없을때도 정상적으로 변환 확인.")
-     public void spritTest02() throws Exception {
-
-         String[] split = "1".split(",");
-
-         Assertions.assertThat(split).containsExactly("1");
-
-      }
-
-      @Test
-      @DisplayName("substring 테스트")
-      public void substringTest() throws Exception {
-
-          String ex = "abc";
-          String result = "abc".substring(1, ex.length() - 1);
-          Assertions.assertThat(result).isEqualTo("b");
-
-       }
-
-
 
 
 
