86958dfea41d84fe00927b8536bf58582e105634
min-gui <msj349911@gmail.com>
2024-09-17 23:12:52 +0900

    fix : 레이싱 생성 팩토리 추가

--- TDD-Prac/src/main/java/study/controller/RacingCarController.java
+++ TDD-Prac/src/main/java/study/controller/RacingCarController.java
@@ -1,9 +1,11 @@
 package study.controller;
 
+import study.service.RacingCarService;
+
 public class RacingCarController {
 
     public static void main(String[] args) {
-
+        RacingCarService racingCarService = new RacingCarService();
         //주어진 횟수동안 n대의 자동차는 전진 또는 멈출 수 있다.
     }
 
--- TDD-Prac/src/main/java/study/domain/RacingCar.java
+++ TDD-Prac/src/main/java/study/domain/RacingCar.java
@@ -3,12 +3,16 @@ package study.domain;
 import study.strategy.MoveStrategy;
 import study.util.RandomCountGenerator;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class RacingCar {
 
     public static int DEFAULT_MOVE_NUM = 4;
     private int position;
 
-    public RacingCar(int position) {
+    public void setPosition(int position) {
         this.position = position;
     }
 
@@ -23,6 +27,12 @@ public class RacingCar {
 
     public int createRandomNum(){
         return RandomCountGenerator.generateRandomNum();
+    }
+
+    public static List<RacingCar> createRacingCars(int cnt){
+        return Stream.generate(RacingCar::new)
+                .limit(cnt)
+                .collect(Collectors.toList());
     }
 
 
